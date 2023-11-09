1b4d353511bc6307ebf819e83dd62bbd8bb41b7f
djai04 <danselmo@correo.um.edu.uy>
2023-11-09 13:26:40 -0300

    cree el esquema de pasajero

---
+++ src/main/java/com/example/sgaapplication/persistency/RepositoryPasajero.java
@@ -0,0 +1,5 @@
+package com.example.sgaapplication.persistency;
+
+public class RepositoryPasajero {
+    
+}
---
+++ src/main/java/com/example/sgaapplication/services/pasajero/ServicePasajero.java
@@ -0,0 +1,5 @@
+package com.example.sgaapplication.services.pasajero;
+
+public class ServicePasajero {
+    
+}
